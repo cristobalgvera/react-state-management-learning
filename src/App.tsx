f599a37cb4b33b13de63b94afacac184ba75cc55
Cristóbal Gajardo Vera <cgajardo@baufest.com>
2021-11-18 18:26:38 -0300

    :lipstick: Update on principal layour UI
    Was changed element position and padding inside the layout.

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Layout>
       <Header title="Gestión de estados" subtitle="Página de bienvenida" />
-      <main className="row-span-3 grid grid-cols-5 gap-x-16 mt-4 justify-center">
+      <main className="row-span-3 grid grid-cols-5 gap-x-16 mt-4 justify-center items-center">
         <CodeBlock codeLines={codeLines} />
         <Implementation>
           <p className="text-3xl font-semibold">Implementación</p>
