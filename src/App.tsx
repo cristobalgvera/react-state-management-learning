2d62a645c8095b79ae41507d861dc0aacf2e25c3
Cristóbal Gajardo Vera <cgajardo@baufest.com>
2021-11-22 16:14:21 -0300

    :lipstick: Changes way to show code to MD files

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import { Header } from './components/Header';
 import { Implementation } from './components/Implementation';
 import { Layout } from './components/Layout';
 import { Logo } from './components/Logo';
-import { useCodeLines } from './hooks/useCodeLines';
+import { useCodeMarkdown } from './hooks/useCodeMarkdown';
 
 function App() {
-  const codeLines = useCodeLines('welcome');
+  const codeMarkdown = useCodeMarkdown('welcome');
 
   return (
     <Layout>
       <Header title="Gestión de estados" subtitle="Página de bienvenida" />
       <main className="row-span-3 grid grid-cols-5 gap-x-16 mt-4 justify-center items-center">
-        <CodeBlock codeLines={codeLines} />
+        <CodeBlock codeMarkdown={codeMarkdown} />
         <Implementation>
           <p className="text-3xl font-semibold">Implementación</p>
           <Logo />
